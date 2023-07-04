f1df7f33d4bbf3a849aa4baff2a3d7493857229e
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-07-04 02:19:19 +0200

    Add files via upload

---
+++ MyJavaSweeper/src/sweeper/Flag.java
@@ -0,0 +1,65 @@
+package sweeper;
+
+//import org.w3c.dom.ranges.Range;
+//import java.awt.*;
+
+public class Flag {
+    private Matrix flagMap;
+    private int countOfClosedBoxes;
+
+    void start() {
+        flagMap = new Matrix(Box.CLOSED);
+        countOfClosedBoxes = Ranges.getSize().x * Ranges.getSize().y;
+    }
+
+    Box get (Coord coord) {
+        return flagMap.get(coord);
+    }
+
+    void setOpenedToBox(Coord coord) {
+            flagMap.set(coord, Box.OPENED);
+            countOfClosedBoxes --;
+    }
+
+    void toggleFlagedToBox(Coord coord) {
+        switch (flagMap.get(coord))
+        {
+            case FLAGED: setClosedToBox(coord); break;
+            case CLOSED: setFlagedToBox(coord);
+        }
+    }
+
+    private void setClosedToBox(Coord coord) {
+        flagMap.set(coord, Box.CLOSED);
+    }
+
+    private void setFlagedToBox(Coord coord) {
+        flagMap.set(coord, Box.FLAGED);
+    }
+
+    int getCountOfClosedBoxes() {
+        return countOfClosedBoxes;
+    }
+
+    public void setBombedToBox(Coord coord) {
+        flagMap.set(coord, Box.BOMBED);
+    }
+
+    void setOpenedToCloseBombBox(Coord coord) {
+        if (flagMap.get (coord) == Box.CLOSED)
+            flagMap.set (coord, Box.OPENED);
+    }
+
+    void setNobombToFlagedSafeBox(Coord coord) {
+        if (flagMap.get(coord) == Box.FLAGED)
+            flagMap.set(coord, Box.NOBOMB);
+    }
+
+    int getCountOfFlagedBoxesAround(Coord coord) {
+        int count = 0;
+        for ( Coord around : Ranges.getCoordsAround(coord))
+            if (flagMap.get(around) == Box.FLAGED)
+                count++;
+        return count;
+    }
+}
