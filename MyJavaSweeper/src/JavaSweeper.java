b528e1c0998705cb09f82168af95bcc33de4635d
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-07-04 17:12:54 +0200

    Update JavaSweeper.java

--- MyJavaSweeper/src/JavaSweeper.java
+++ MyJavaSweeper/src/JavaSweeper.java
@@ -8,8 +8,7 @@ import sweeper.Coord;
 import sweeper.Game;
 import sweeper.Ranges;
 
-public class JavaSweeper extends JFrame
-{
+public class JavaSweeper extends JFrame {
     private Game game;
     private JPanel panel;
     private JLabel label;
@@ -19,13 +18,11 @@ public class JavaSweeper extends JFrame
     private final int BOMBS = 25;
     private final int IMAGE_SIZE = 50;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         new JavaSweeper().setVisible(true);
     }
 
-    private JavaSweeper()
-    {
+    private JavaSweeper() {
         game = new Game(COLS, ROWS, BOMBS);
         game.start();
         setImages();
@@ -34,19 +31,15 @@ public class JavaSweeper extends JFrame
         initFrame();
     }
 
-    private void initLabel()
-    {
+    private void initLabel() {
         label = new JLabel("Welcome!");
         add (label, BorderLayout.SOUTH);
     }
 
-    private void initPanel()
-    {
-        panel = new JPanel()
-        {
+    private void initPanel() {
+        panel = new JPanel() {
             @Override
-            protected void paintComponent(Graphics g)
-            {
+            protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 for (Coord coord : Ranges.getAllCoords())
 
@@ -55,11 +48,9 @@ public class JavaSweeper extends JFrame
             }
         };
 
-        panel.addMouseListener(new MouseAdapter()
-        {
+        panel.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e)
-            {
+            public void mousePressed(MouseEvent e) {
                 int x = e.getX() / IMAGE_SIZE;
                 int y = e.getY() / IMAGE_SIZE;
                 Coord coord = new Coord(x, y);
@@ -80,10 +71,8 @@ public class JavaSweeper extends JFrame
         add(panel);
     }
 
-    private String getMessage()
-    {
-        switch (game.getState())
-        {
+    private String getMessage() {
+        switch (game.getState()) {
             case PLAYED: return "Think twice!";
             case BOMBED: return "YOU LOSE! BIG BA-DA-BOOM !!!";
             case WINNER: return "CONGRATULATIONS!";
@@ -91,8 +80,7 @@ public class JavaSweeper extends JFrame
         }
     }
 
-    private void initFrame()
-    {
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
         setResizable(false);
@@ -102,14 +90,12 @@ public class JavaSweeper extends JFrame
         setIconImage(getImage("icon"));
     }
 
-    private void setImages()
-    {
+    private void setImages() {
         for (Box box : Box.values())
             box.image = getImage(box.name().toLowerCase());
     }
 
-    private Image getImage (String name)
-    {
+    private Image getImage (String name) {
         String filename = "img/" + name + ".png";
         ImageIcon icon = new ImageIcon(getClass().getResource(filename));
         return icon.getImage();
